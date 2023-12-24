a9109bff44df550f7e47a361fa851420fbc8399f
Parisa Karimi <154719698+parisaJan@users.noreply.github.com>
2023-12-24 17:11:34 +0300

    Add files via upload
    Signed-off-by: Parisa Karimi <154719698+parisaJan@users.noreply.github.com>

---
+++ resnet.pytorch.py
@@ -0,0 +1,5 @@
+import torch
+from torchvision.models import resnet18, ResNet18_Weights
+model = resnet18(weights=ResNet18_Weights.DEFAULT)
+data = torch.rand(1, 3, 64, 64)
+labels = torch.rand(1, 1000)
